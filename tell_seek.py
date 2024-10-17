074a4c0b011e9f5e8cf8f78d057362ea8ad4e072
bhattvrunda <vrundabhatt512@gmail.com>
2024-10-17 11:22:22 +0530

    Delete tell_seek.py

--- tell_seek.py
+++
@@ -1,8 +0,0 @@
-import sys
-myfile=open(sys.argv[0],'r+')
-for line in myfile:
-    modline=line.replace('mispelt','miispelt')
-    curoff=myfile.tell()
-    myfile.seek(curoff-len(line))
-    print(modline,file=myfile,end='')
-    myfile.close()
