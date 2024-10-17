36f40106c23846819313be7ea8a14860095909f4
bhattvrunda <vrundabhatt512@gmail.com>
2024-10-17 11:34:38 +0530

    Delete find_email.py

--- find_email.py
+++
@@ -1,6 +0,0 @@
-import re
-
-text="My department email id is rupesh@cse.iitm.ac.in.This is what I use for all the official purpose.During phd i used rupesh)something@gmail.com,when gmail was not around rupesh_something@usa.net
-regex=r'\w+@\w+'
-email=re.findall(regex,text.lower())
-print(email)
